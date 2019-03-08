fc627587ad1b812f3d4adb526627da8a2a33b7e6
Hano <hinsul3113@gmail.com>
2019-03-08 15:27:26 -0800

    created weatherReducer and sent a request over weathermap api to get forecast for the searched city

---
+++ src/reducers/weatherReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATHER } from '../actions/types';
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [...state, action.payload.data];
+      
+    default:
+      return state;
+  }
+}
