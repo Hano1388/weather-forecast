493501c6eed6aeb4e53dbcc0abec1c57e1add75d
Hano <hinsul3113@gmail.com>
2019-03-08 17:05:12 -0800

    added a small map for each city and formatted elements on page

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/Chart';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
+import GoogleMap from '../components/GoogleMap';
 
 
 class WeatherList extends Component {
@@ -17,9 +18,11 @@ class WeatherList extends Component {
       weather => weather.main.humidity
     );
 
+    const { lon, lat } = cityData.city.coord;
+
     return (
       <tr key={id}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temperatures} color="orange" units="K"/></td>
         <td><Chart data={pressures} color="green" units="hPa"/></td>
         <td><Chart data={humidities} color="black" units="%"/></td>
